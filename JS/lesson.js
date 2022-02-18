eccf767f6e6a95e2abcdb5c0e786b5c19f9687da
Sam Morningstar <sammorningstar5064@gmail.com>
2022-02-18 14:58:02 +0400

    Data

---
+++ JS/lesson.js
@@ -0,0 +1,107 @@
+let app = new Vue({
+    data: {
+      sitename: "Lessons",
+      lessons: [
+        {
+          id: 01,
+          subject: "English",
+          location: "London",
+          price: 50,
+          space: 5,
+          image: "Assets/English.jpg",
+          icon: "fas fa-pen",
+        },
+  
+        {
+          id: 02,
+          subject: "Math",
+          location: "York",
+          price: 55,
+          space: 5,
+          image: "Assets/Math.jpeg",
+          icon: "fas fa-plus",
+        },
+  
+        {
+          id: 03,
+          subject: "Biology",
+          location: "Bristol",
+          price: 45,
+          space: 5,
+          image: "Assets/Bio.jpg",
+          icon: "fas fa-seedling",
+        },
+  
+        {
+          id: 04,
+          subject: "Physics",
+          location: "York",
+          price: 65,
+          space: 5,
+          image: "Assets/Phy.jpg",
+          icon: "fas fa-atom",
+        },
+  
+        {
+          id: 05,
+          subject: "Chemistry",
+          location: "Manchester",
+          price: 85,
+          space: 5,
+          image: "Assets/Chem.jpg",
+          icon: "fas fa-flask",
+        },
+  
+        {
+          id: 06,
+          subject: "Computer Science",
+          location: "Birmingham",
+          price: 95,
+          space: 5,
+          image: "Assets/CompSc.jpg",
+          icon: "fas fa-mouse-pointer",
+        },
+  
+        {
+          id: 07,
+          subject: "Business",
+          location: "London",
+          price: 75,
+          space: 5,
+          image: "Assets/Business.jpg",
+          icon: "fas fa-briefcase",
+        },
+  
+        {
+          id: 08,
+          subject: "Accountancy",
+          location: "Birmingham",
+          price: 60,
+          space: 5,
+          image: "Assets/Accounting.jpg",
+          icon: "fas fa-calculator",
+        },
+  
+        {
+          id: 09,
+          subject: "Economics",
+          location: "Manchester",
+          price: 80,
+          space: 5,
+          image: "Assets/Eco.jpg",
+          icon: "fas fa-money-bill-wave",
+        },
+  
+        {
+          id: 10,
+          subject: "Psychology",
+          location: "Liverpool",
+          price: 50,
+          space: 5,
+          image: "Assets/Psy.jpg",
+          icon: "fas fa-brain",
+        },
+      ]
+    },
+  });
+  
