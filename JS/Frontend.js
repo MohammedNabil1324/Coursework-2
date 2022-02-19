b1789b759a04538f29395338faf34e12ad211059
Sam Morningstar <sammorningstar5064@gmail.com>
2022-02-19 08:55:07 +0400

    Test

---
+++ JS/Frontend.js
@@ -0,0 +1,15 @@
+let app = new Vue({
+    data: {
+      sitename: "Lessons",
+      lessons:{},
+    },
+    created: function () {
+        fetch("https://coursewrk-2.herokuapp.com/collection/Lessons").then(
+          function (response) {
+            response.json.then(function (json) {
+              app1.lessons = json;
+            });
+          }
+        );
+      },
+    });
--- index.js
+++ index.js
@@ -5,22 +5,6 @@ var mongodb = require("mongodb").MongoClient;
 var app1 = express();
 let db;
 
-let app = new Vue({
-  data: {
-    sitename: "Lessons",
-    lessons:{},
-  },
-  created: function () {
-      fetch("https://coursewrk-2.herokuapp.com/collection/Lessons").then(
-        function (response) {
-          response.json.then(function (json) {
-            app1.lessons = json;
-          });
-        }
-      );
-    },
-  });
-
 const port = process.env.PORT || 3000;
 app1.listen(port);
 
