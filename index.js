543d5a63bc698aa4da97b53955fce0ba5cd9520d
Sam Morningstar <sammorningstar5064@gmail.com>
2022-02-19 06:05:00 +0400

    MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
+const MongoClient = require("mongodb").MongoClient;
+
 var app = express();
+let db;
+MongoClient.connect(
+  "mongodb+srv://ASDF:ASDF@cluster0.2a6e0.mongodb.net/CW?retryWrites=true&w=majority",
+  (err, client) => {
+    db = client.db("webstore");
+  }
+);
 
 app.use(function (req, res, next) {
   console.log("Request IP: " + req.url);
@@ -31,4 +40,20 @@ app.use(function (req, res, next) {
 app.use(function (req, res) {
   res.status(404);
   res.send("File not found!");
+});
+
+app.param("collectionName", (req, res, next, collectionName) => {
+  req.collection = db.collection(collectionName);
+  return next();
+});
+
+app.get("/", (req, res, next) => {
+  res.send("Select a collection");
+});
+
+app.get("/collection/:collectionName", (req, res, next) => {
+  req.collection.find({}).toArray((e, results) => {
+    if (e) return next(e);
+    res.send(results);
+  });
 });
